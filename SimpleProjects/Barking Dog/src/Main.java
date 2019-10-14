ba0583282aa0a6271067240f36ffce32923623ab
Saiful Islam <tosaiful@live.com>
2019-10-14 17:24:24 +0200

    Old projects uploaded

---
+++ SimpleProjects/Barking Dog/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        boolean printResult=  BarkingDog.shouldWakeUp(true, -1);
+        System.out.println(printResult);
+    }
+}
