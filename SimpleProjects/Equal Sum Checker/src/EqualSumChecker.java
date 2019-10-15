287a21adb799fc0195ce2603110760146eb4f89e
Saiful Islam <tosaiful@live.com>
2019-10-15 19:49:53 +0200

    Equal Sum Checker added

---
+++ SimpleProjects/Equal Sum Checker/src/EqualSumChecker.java
@@ -0,0 +1,10 @@
+public class EqualSumChecker {
+
+    public static boolean hasEqualSum(int parOne, int parTwo, int parThree){
+
+        if((parOne + parTwo) == parThree ){
+            return true;
+        }
+        return false;
+    }
+}
