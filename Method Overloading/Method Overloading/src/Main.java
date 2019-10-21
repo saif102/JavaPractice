da075b267b16e600ef3d1652a407464c3f5bb6c4
Saiful Islam <tosaiful@live.com>
2019-10-21 13:44:12 +0200

    method overloading folder created.
    One project added to method overloading folder.

---
+++ Method Overloading/Method Overloading/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        CentimetersCalculator.calcFeetAndInchesToCentimeters(1, 9);
+        CentimetersCalculator.calcFeetAndInchesToCentimeters(100);
+    }
+}
+
