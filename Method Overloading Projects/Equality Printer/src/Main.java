3d0c0613da9551aba5d93d61983c99f1436098ee
Saiful Islam <tosaiful@live.com>
2019-10-22 15:05:15 +0200

    Equality Printer project added

---
+++ Method Overloading Projects/Equality Printer/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        EqualityPrinter.IntEqualityPrinter(1,1,1);
+        EqualityPrinter.IntEqualityPrinter(1,1,2);
+        EqualityPrinter.IntEqualityPrinter(-1,-1,1);
+        EqualityPrinter.IntEqualityPrinter(1,2,3);
+
+    }
+}
