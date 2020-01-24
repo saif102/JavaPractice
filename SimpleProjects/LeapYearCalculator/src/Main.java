e352aa9de516d004b4105d1b518dfa2f81aca684
Saiful Islam <tosaiful@live.com>
2020-01-24 14:22:12 +0100

    newly uploaded

---
+++ SimpleProjects/LeapYearCalculator/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        /*boolean printYear = LeapYearCalculator.isLeapYear(1700);
+        System.out.println(printYear);*/
+
+        System.out.println(LeapYearCalculator.isLeapYear(2020));
+    }
+}
